f33b4972fbc05ec6a1ca996684988bf609af81c1
Abhishek <abhishekthapliyal7579@gmail.com>
2019-01-15 16:51:15 +0530

    chore(Project): Initiating App

--- FoxBankSource/Source/Configurations/wsgi.py
+++ FoxBankSource/Source/Configurations/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for FoxBank project.
+WSGI config for Configurations project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FoxBank.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Configurations.settings")
 
 application = get_wsgi_application()
---
+++ FoxBankSource/Source/FoxBank/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FoxbankappConfig(AppConfig):
+    name = 'FoxBank'
